3a1fdf37963286f38c14d7f1fb88a826f811ef32
ardhika23 <ardhika@gmail.com>
2023-11-16 18:13:21 +0700

    Finished Tutorial 8

--- main/urls.py
+++ main/urls.py
@@ -8,7 +8,7 @@ from main.views import register
 from main.views import login_user
 from main.views import logout_user
 from main.views import edit_product
-from main.views import delete_product
+from main.views import delete_product, create_product_flutter
 
 app_name = 'main'
 
@@ -25,5 +25,6 @@ urlpatterns = [
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('delete/<int:id>', delete_product, name='delete_product'), 
     path('get-product/', get_product_json, name='get_product_json'),
-    path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
